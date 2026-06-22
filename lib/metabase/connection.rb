be5989fba42be5dde259c2f6b96d55920fa4b46d
Evgeny Vlasov <e.vlasov@foxford.ru>
2026-06-22 17:07:17 +0300

    STOEGE-28668 Fix vulnerable gem versions

--- lib/metabase/connection.rb
+++ lib/metabase/connection.rb
@@ -5,6 +5,8 @@ require 'metabase/error'
 
 module Metabase
   module Connection
+    QUERY_PARAM_METHODS = %i[get delete head].freeze
+
     def get(path, **params)
       request(:get, path, params)
     end
@@ -28,20 +30,43 @@ module Metabase
     private
 
     def request(method, path, params)
-      headers = params.delete(:headers)
+      headers = request_headers(params.delete(:headers))
       query_params = params.delete(:query_params) || {}
-      query_params[:format_rows] = params.delete(:format_rows)
 
-      response = connection.public_send(method, path, params) do |request|
-        request.headers['X-Metabase-Session'] = @token if @token
-        headers&.each_pair { |k, v| request.headers[k] = v }
-        request.params = query_params.compact
+      response = connection.public_send(method, path) do |request|
+        request.headers.update(headers)
+
+        if query_param_method?(method)
+          configure_query_request(request, params, query_params)
+        else
+          configure_body_request(request, params, query_params)
+        end
       end
 
       error = Error.from_response(response)
       raise error if error
 
       response.body
+    end
+
+    def request_headers(headers)
+      { 'X-Metabase-Session' => @token }.compact.merge(headers || {})
+    end
+
+    def configure_query_request(request, params, query_params)
+      request.params.update(params.merge(query_params.compact))
+    end
+
+    def configure_body_request(request, params, query_params)
+      query_params = query_params.compact
+      request.params.update(query_params) if query_params.any?
+
+      body = params.compact
+      request.body = body if body.any?
+    end
+
+    def query_param_method?(method)
+      QUERY_PARAM_METHODS.include?(method)
     end
 
     def connection
--- spec/metabase/connection_spec.rb
+++ spec/metabase/connection_spec.rb
@@ -7,11 +7,53 @@ RSpec.describe Metabase::Connection do
     include_examples 'response handling' do
       let(:method) { :get }
     end
+
+    context 'with params' do
+      before do
+        stub_request(:get, host)
+          .with(query: { 'entity' => 'card', 'id' => '1' })
+          .to_return(status: 200, body: 'OK')
+      end
+
+      it 'keeps params in the query string' do
+        expect(client.get(path, entity: :card, id: 1)).to eq('OK')
+      end
+    end
   end
 
   describe 'post' do
     include_examples 'response handling' do
       let(:method) { :post }
+    end
+
+    context 'with false body params' do
+      before do
+        stub_request(:post, host)
+          .with do |request|
+            request.uri.query.nil? &&
+              request.body.include?('"format_rows":false')
+          end
+          .to_return(status: 200, body: 'OK')
+      end
+
+      it 'keeps params in the request body' do
+        expect(client.post(path, format_rows: false)).to eq('OK')
+      end
+    end
+
+    context 'with true body params' do
+      before do
+        stub_request(:post, host)
+          .with do |request|
+            request.uri.query.nil? &&
+              request.body.include?('"format_rows":true')
+          end
+          .to_return(status: 200, body: 'OK')
+      end
+
+      it 'keeps params in the request body' do
+        expect(client.post(path, format_rows: true)).to eq('OK')
+      end
     end
   end
 
